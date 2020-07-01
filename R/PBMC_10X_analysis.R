1e5166f4ac1d8ba16e0ed8464fac34d8dbd4eb7a
biocaitao <tao.cai@gmail.com>
2020-07-01 11:08:55 +0800

    Update PBMC_10X_analysis.R

--- R/PBMC_10X_analysis.R
+++ R/PBMC_10X_analysis.R
@@ -1,9 +1,7 @@
-#PBMC1.R
 library(dplyr)
 library(Seurat)
 setwd("~/data/CaiT/PBMC/2019_3_12/")
-#load("pbmc.dat")
-pbmc.data <- Read10X(data.dir = "PBMC2/outs/filtered_feature_bc_matrix/")
+pbmc.data <- Read10X(data.dir = "PBMC/outs/filtered_feature_bc_matrix/")
 pbmc <- CreateSeuratObject(counts = pbmc.data, min.cells = 3, min.features = 200,project = "10X_PBMC")
 pbmc
 mito.features <- grep(pattern = "^MT-", x = rownames(x = pbmc), value = TRUE)
@@ -13,9 +11,9 @@ VlnPlot(object = pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"
 pbmc <- subset(x = pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2000 & percent.mito < 0.05)
 pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize",scale.factor = 10000)
 #pbmc <- FindVariableFeatures(object = pbmc, selection.method = "mean.var.plot",mean.cutoff = c(0.0125, 4), dispersion.cutoff = c(0.5, Inf))
+#test VST 
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
 VariableFeaturePlot(object = pbmc)
-#pbmc <- ScaleData(object = pbmc, features = VariableFeatures(pbmc), vars.to.regress = c("nCount_RNA"))
 pbmc <- ScaleData(pbmc)
 pbmc <- RunPCA(object = pbmc, features = VariableFeatures(object = pbmc))
 #pbmc <- JackStraw(pbmc, num.replicate = 100)
