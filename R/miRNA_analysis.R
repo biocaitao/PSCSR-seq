64d362414c635e87b337125f717c313ce9e71cd9
biocaitao <tao.cai@gmail.com>
2020-07-27 18:26:50 +0800

    Update miRNA_analysis.R

--- R/miRNA_analysis.R
+++ R/miRNA_analysis.R
@@ -17,17 +17,18 @@ cells <- subset(x = cells, cells=rownames(cells2@meta.data) )
 VlnPlot(cells, c("nCount_RNA", "nFeature_RNA") )
 
 cells <- NormalizeData(object = cells, normalization.method = "LogNormalize",scale.factor = 10000)
-cells <- FindVariableFeatures(object = cells, selection.method = "mean.var.plot",mean.cutoff = c(0.00125, Inf), dispersion.cutoff = c(0.1, Inf), num.bin = 5)
+#cells <- FindVariableFeatures(object = cells, selection.method = "mean.var.plot",mean.cutoff = c(0.00125, Inf), dispersion.cutoff = c(0.1, Inf), num.bin = 5)
+cells <- FindVariableFeatures(object = cells, selection.method = "vst", nfeatures=400, num.bin = 10)
 VariableFeaturePlot(object = cells)
 length(x = VariableFeatures(object = cells))
-cells <- ScaleData(object = cells,  vars.to.regress = c("nCount_RNA"))
+cells <- ScaleData(object = cells)
 cells <- RunPCA(object = cells, features = VariableFeatures(cells) ,npcs = 150)
 ElbowPlot(object = cells,ndims = 150)
 cells <- RunTSNE(object = cells, dims = 1:50)
 DimPlot(object = cells, reduction = "tsne", pt.size = 2)
 
 cells <- FindNeighbors(object = cells, dims = 1:50)
-cells <- FindClusters(object = cells, resolution =seq(0.1,1, 0.1))
+cells <- FindClusters(object = cells, resolution =seq(0.1,2, 0.1))
 
 Idents(object = cells)<-'RNA_snn_res.0.5'
 DimPlot(object = cells, reduction = "tsne",label = T)
@@ -40,9 +41,9 @@ cells.markers <- FindAllMarkers(object = cells, only.pos = TRUE, min.pct = 0.5, 
 
 #####
 #heatmap for the detail relations
-data<-GetAssayData(object = cells, slot = "data")
+data<-GetAssayData(object = cells, slot = "count")
 dim(data[VariableFeatures(cells),])
 library(pheatmap)
 library(RColorBrewer)
 m<-as.matrix(data[VariableFeatures(cells),])
-pheatmap(m ,clustering_method = "ward.D2", show_colnames = F, fontsize_row = 1)
+pheatmap(m ,clustering_method = "complete", show_colnames = F, fontsize_row = 1)
